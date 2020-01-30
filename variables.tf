9377fb235facab8cff405804387e153ff4aac2fc
Daniel Balcomb <daniel.balcomb@gmail.com>
2020-01-30 13:52:42 +0000

    Add azure container registry resource (#3)

--- variables.tf
+++ variables.tf
@@ -1,9 +1,20 @@
 variable "name" {
-  description = "The resource name"
+  description = "The registry name"
   type        = string
 }
 
 variable "location" {
-  description = "The resource location"
+  description = "The registry location"
+  type        = string
+}
+
+variable "sku" {
+  description = "The registry SKU"
+  default     = "Basic"
+  type        = string
+}
+
+variable "dns_prefix" {
+  description = "The registry DNS prefix"
   type        = string
 }
