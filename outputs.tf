c477f0fd1798250fee7f8a6e83b495fdc22a0b5f
Daniel Balcomb <daniel.balcomb@gmail.com>
2020-01-30 13:50:06 +0000

    Add azure container registry resource

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,24 @@
+output "id" {
+  description = "The registry ID"
+  value       = azurerm_container_registry.main.id
+}
+
+output "url" {
+  description = "The registry URL"
+  value       = azurerm_container_registry.main.login_server
+}
+
+output "username" {
+  description = "The admin username"
+  value       = azurerm_container_registry.main.admin_username
+}
+
+output "password" {
+  description = "The admin password"
+  value       = azurerm_container_registry.main.admin_password
+  sensitive   = true
+}
+
 output "resource_group" {
   description = "The resource group"
   value       = azurerm_resource_group.main
