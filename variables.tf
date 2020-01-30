64c7df8fa2a825e4c4a8a5e418fc30c079bf32c7
Daniel Balcomb <daniel.balcomb@gmail.com>
2020-01-30 15:30:57 +0000

    Add resource tags (#6)

--- variables.tf
+++ variables.tf
@@ -18,3 +18,27 @@ variable "dns_prefix" {
   description = "The registry DNS prefix"
   type        = string
 }
+
+variable "environment" {
+  description = "The environment name"
+  default     = "production"
+  type        = string
+}
+
+variable "provisioner" {
+  description = "The provisioner name"
+  default     = "terraform"
+  type        = string
+}
+
+variable "stack" {
+  description = "The stack name"
+  default     = "acr"
+  type        = string
+}
+
+variable "tags" {
+  description = "The resource tags"
+  default     = {}
+  type        = map(string)
+}
